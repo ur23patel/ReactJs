01b1b9fd416acaf3c54f3de45ac67738f3400755
Urvi Patel <118416708+ur23patel@users.noreply.github.com>
2023-10-25 11:00:16 -0400

    Update 02fucntionalcompointro.jsx

--- ReactJS/topics/src/Components/FunctionalComponent/02fucntionalcompointro.jsx
+++ ReactJS/topics/src/Components/FunctionalComponent/02fucntionalcompointro.jsx
@@ -3,12 +3,11 @@ import React from 'react';
 const fucntionalcompointro = () => {
     return (
         <>
+            <p>This function is a valid React component because it accepts a single “props” (which stands for properties) object argument with data and returns a React element. We call such components “function components” because they are literally JavaScript functions.</p>
             <pre>
-            class Welcome extends React.Component &#123; <br />
-                &nbsp;&nbsp; render() &#123;<br />
-                &nbsp;&nbsp;&nbsp;&nbsp;return &lt;h1&gt;Hello, &#123;this.props.name&#125;&lt;/h1&gt;;<br />
-                &nbsp;&nbsp;&#125;<br />
-            &#125;<br />
+                function Welcome(props) &#123; <br/>
+                   &nbsp;&nbsp;&nbsp;&nbsp; return &lt;h1&gt;Hello, &#123;props.name&#125;&lt;/h1&gt;; <br/>
+                &#125; <br/>
             </pre>
         </>
     );
