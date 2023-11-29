e96c3b05abfd2b15222f30b6ffdcb57eb7486144
Urvi Patel <118416708+ur23patel@users.noreply.github.com>
2023-11-29 11:27:58 -0500

    Update FunctionalComponentRoute.jsx

--- ReactJS/topics/src/Routes/FunctionalComponentRoute.jsx
+++ ReactJS/topics/src/Routes/FunctionalComponentRoute.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import FunctionalCompoMenu from './../Components/FunctionalComponent/01FunctionalCompoMenu.jsx';
 import FunctionalComponentIntro from './../Components/FunctionalComponent/02fucntionalcompointro.jsx';
+import FunctionalComponentState from './../Components/FunctionalComponent/03FunctionalComponentState.jsx';
+import FunctionalComponentProps from './../Components/FunctionalComponent/04FunctionalComponentProps.jsx';
+import FunctionalComponentUseEffect from './../Components/FunctionalComponent/05FunctionalComponentUseEffect.jsx';
+import FunctionalComponentUseLayoutEffect from './../Components/FunctionalComponent/06FunctionalComponentUseLayoutEffect.jsx';
+import FunctionalComponentUseEffectUseLayoutEffect from './../Components/FunctionalComponent/07FunctionalComponentUseEffectUseLayoutEffect.jsx';
+import FunctionalComponentUseMemo from './../Components/FunctionalComponent/08FunctionalComponentUseMemo.jsx';
+import FunctionalComponentUseCallback from './../Components/FunctionalComponent/09FunctionalComponentUseCallback.jsx';
+import FunctionalCompoUseMemoFactorialExample from './../Components/FunctionalComponent/11FunctionalCompoUseMemoExample.jsx';
+import FunctionalUseContext from './../Components/FunctionalComponent/UseContextExmaple/12FunctionalUseContext.jsx';
+import FunctionalImperativeHandle from './../Components/FunctionalComponent/ImperativeHandle/13FunctionalImperativeHandle.jsx';
+import FunctionaluseRef from './../Components/FunctionalComponent/14useRef.jsx';
+import FunctionalUseReducer from './../Components/FunctionalComponent/15FunctionalUseReducer.jsx';
+import FunctionalHonyComTask from './../Components/FunctionalComponent/HonyComTask/userImperativeMain.jsx';
 
 const FunctionalComponentRoute = () => {
     const routes = useRoutes([
@@ -12,10 +25,48 @@ const FunctionalComponentRoute = () => {
                 {
                     path: "functionalcompointro",
                     element: <FunctionalComponentIntro />,
-                },
-                {
+                },{
                     path: "fucntionalhooks",
                     element: <FunctionalComponentIntro />,
+                },{
+                    path: "functionalcompostate",
+                    element: <FunctionalComponentState />,
+                },{
+                    path: "functionalcompoprops",
+                    element: <FunctionalComponentProps />,
+                },{
+                    path: "functionalcompouseeffect",
+                    element: <FunctionalComponentUseEffect />,
+                },{
+                    path: "functionalcompouselayouteffect",
+                    element: <FunctionalComponentUseLayoutEffect />,
+                },{
+                    path: "functionalcompouseeffectuselayouteffect",
+                    element: <FunctionalComponentUseEffectUseLayoutEffect />,
+                },{
+                    path: "functionalcompousememo",
+                    element: <FunctionalComponentUseMemo />,
+                },{
+                    path: "functionalcompousecallback",
+                    element: <FunctionalComponentUseCallback />,
+                },{
+                    path: "functionalcompousememoexample",
+                    element: <FunctionalCompoUseMemoFactorialExample />,
+                },{
+                    path: "functionalcompousecontext",
+                    element: <FunctionalUseContext />,
+                },{
+                    path: "functionalimperativehandle",
+                    element: <FunctionalImperativeHandle />,
+                },{
+                    path: "functionaluseref",
+                    element: <FunctionaluseRef />,
+                },{
+                    path: "functionalusereducer",
+                    element: <FunctionalUseReducer />,
+                },{
+                    path: "honycomtask",
+                    element: <FunctionalHonyComTask />,
                 },
             ]
         }
